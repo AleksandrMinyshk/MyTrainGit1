26431267d599ad7a4ed1fec5874e8758c48cc15f
AleksandrMinyshk <alexmech001@gmail.com>
2023-04-03 21:09:32 -0400

    V.1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+            BasicStructure audi =new BasicStructure();
+            audi.color="balck";
+            audi.model="A3";
+            audi.power="5000cc";
+
+            Engine X =new Engine();
+            X.cylinders="8";
+            X.size="1234cc";
+            X.weight="6453k";
+    }
+}
